a091d4a2613ba86f5cf0320a0ef07bd589fa3b63
Aiden <aiden.gimpel@ontariotechu.net>
2021-08-06 14:45:59 -0400

    Added Image Changing to the level complete box to make it look like game

--- GameDevBros3Example/Assets/Scripts/Gameplay/LevelCompleteSquare.cs
+++ GameDevBros3Example/Assets/Scripts/Gameplay/LevelCompleteSquare.cs
@@ -7,6 +7,12 @@ public class LevelCompleteSquare : MonoBehaviour
     [SerializeField] List<Sprite> powerupList;
     [SerializeField] GameObject gameManager;
 
+    float changeTimer = 0;
+
+    int previousChoice = 5;
+
+    bool itemCollected;
+
     void OnTriggerEnter2D(Collider2D collision)
     {
         gameManager.GetComponent<ScoreCounter>().AddScore(gameManager.GetComponent<Timer>().GetTimeLeft() * 100);
@@ -15,5 +21,39 @@ public class LevelCompleteSquare : MonoBehaviour
 
         FindObjectOfType<AudioManager>().Stop("Music");
         FindObjectOfType<AudioManager>().Play("LevelClear");
+
+        itemCollected = true;
+    }
+
+    void Update()
+    {
+        if (!itemCollected)
+        {
+            ChangeImage();
+        }
+    }
+
+    void ChangeImage()
+    {
+        if (changeTimer < Time.realtimeSinceStartup)
+        {
+            int choice = (int)Random.Range(0, 5);
+
+            if (choice == 5)
+            {
+                choice = 4;
+            }
+
+            if (choice == previousChoice)
+            {
+                choice++;
+            }
+
+            GetComponent<SpriteRenderer>().sprite = powerupList[choice];
+
+            changeTimer = Time.realtimeSinceStartup + 0.25f;
+
+            previousChoice = choice;
+        }
     }
 }
